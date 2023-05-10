0ebdb46986a2e6805514e0f087e740e023dce5a2
Masaya Ohtsuka <crystarmen125@gmail.com>
2023-05-10 13:05:08 +0000

    2nd

---
+++ app/controllers/list_controller.rb
@@ -0,0 +1,24 @@
+class ListController < ApplicationController
+  def new
+    @list=List.new
+  end
+def create
+  list=List.new(list_params)
+  list.save
+end
+
+  def index
+    @list=List.all
+  end
+
+  def show
+    @list=List.find(params[:id])
+  end
+
+  def edit
+  end
+ private
+ def list_params
+   params.require(:list).parmit(:title, :body)
+ end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
+  get 'list/new'
+  post 'list'=>'list#create'
+  get 'list' =>  'list#index'
+  get 'lists/:id' => 'list#show', as: 'list'
+  get 'list/edit'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
