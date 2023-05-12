0450a038174d8590daef5a40f66574a51705ae11
Masaya Ohtsuka <crystarmen125@gmail.com>
2023-05-12 13:28:20 +0000

    4th

--- app/controllers/list_controller.rb
+++ app/controllers/list_controller.rb
@@ -3,8 +3,14 @@ class ListController < ApplicationController
     @list=List.new
   end
 def create
-  list=List.new(list_params)
-  list.save
+  @list=List.new(list_params)
+  if@list.save
+    flash[:notice] = "投稿が成功しました"
+  redirect_to list_path(@list.id)
+  else
+    @lists=List.all
+  rendar :index
+  end
 end
 
   def index
@@ -16,6 +22,12 @@ end
   end
 
   def edit
+    @list=List.find(params[:id])
+  end
+  def destroy
+    list =  List.find(params[:id])
+    list.destroy
+    redirect_to '/lists'
   end
  private
  def list_params
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,6 @@
 Rails.application.routes.draw do
+  get 'top' => 'homes#top'
   get 'list/new'
    # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  post 'list'=>'list#create'
-  get 'list' =>  'list#index'
-  get 'lists/:id' => 'list#show', as: 'list'
-  get 'lists/:id/edit' => 'lists#edit', as: 'edit_list'
- patch 'lists/:id'=> 'lists#update', as: 'update_list'
+  resources :lists
 end
